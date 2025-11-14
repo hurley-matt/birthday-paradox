50a1e37bea3651a03f9126c451786b86791fab1a
Matt <hurley.matt@proton.me>
2025-11-14 18:44:33 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import datetime
+
+
+# numbers_of_birthdays = input("How many birthdays shall I generate? (100 max): ")
+
+# for birthday in range(numbers_of_birthdays):
+
+print(datetime.date.today())
