4868fe831551301f32e6bc4018c71f77c532b933
Matt <hurley.matt@proton.me>
2025-11-15 14:41:44 -0500

    Add month an date randomization

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import datetime
+import calendar
+import random
+
+numbers_of_birthdays = int(input("How many birthdays shall I generate? (100 max): "))
+
+birthdays = []
+
+for birthday in range(numbers_of_birthdays):
+    random_month = random.randint(1, 12)
+    month = calendar.month_abbr[random_month]
+    max_date = calendar.monthrange(2025, random_month)
+    date = random.randint(1, max_date[1])
+    birthday = f"{month}, {date}"
+    birthdays.append(birthday)
 
 
-# numbers_of_birthdays = input("How many birthdays shall I generate? (100 max): ")
-
-# for birthday in range(numbers_of_birthdays):
-
-print(datetime.date.today())
